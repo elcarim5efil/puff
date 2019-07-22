74e922a96a8b277bef81340b6d5acceb46f224e0
elcarim5efil <elcarim5efil@gmail.com>
2019-07-22 17:59:32 +0800

    test: add test for resolve-module-info-external

---
+++ test/units/resolve-module-info-external.test.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const {
+  isExternalModule,
+  resolveModuleInfoExternal,
+} = require('../../src/resolve-module-info-external');
+
+describe('isExternalModule', () => {
+  test('./module', () => {
+    expect(isExternalModule('./module')).toBeFalsy();
+  });
+
+  test('/module', () => {
+    expect(isExternalModule('/module')).toBeFalsy();
+  });
+
+  test('module', () => {
+    expect(isExternalModule('module')).toBeTruthy();
+  });
+
+  test('@/module', () => {
+    expect(isExternalModule('@/module')).toBeTruthy();
+  });
+
+  test('@module', () => {
+    expect(isExternalModule('@module')).toBeTruthy();
+  });
+
+  test('@module/xxx', () => {
+    expect(isExternalModule('@module/xxx')).toBeTruthy();
+  });
+
+  test('module/xxx', () => {
+    expect(isExternalModule('module/xxx')).toBeTruthy();
+  });
+});
+
+
+describe('resolveModuleInfoExternal', () => {
+  test('resolve dependencies lodash', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/');
+    const res = resolveModuleInfoExternal('lodash', root);
+    expect(res).toEqual({
+      requireName: 'lodash',
+      moduleName: 'lodash',
+      dependencyType: 'dependencies',
+      version: '^4.17.14',
+    });
+  });
+
+  test('resolve dependencies lodash/map', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/');
+    const res = resolveModuleInfoExternal('lodash/map', root);
+    expect(res).toEqual({
+      requireName: 'lodash/map',
+      moduleName: 'lodash',
+      dependencyType: 'dependencies',
+      version: '^4.17.14',
+    });
+  });
+
+  test('resolve peerDependencies', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/');
+    const res = resolveModuleInfoExternal('webpack', root);
+    expect(res).toEqual({
+      requireName: 'webpack',
+      moduleName: 'webpack',
+      dependencyType: 'peerDependencies',
+      version: '5',
+    });
+  });
+
+  test('resolve devDependencies', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/');
+    const res = resolveModuleInfoExternal('vuex', root);
+    expect(res).toEqual({
+      requireName: 'vuex',
+      moduleName: 'vuex',
+      dependencyType: 'devDependencies',
+      version: 'latest',
+    });
+  });
+});
