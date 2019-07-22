e35e3cb8cf47cdcff824dbdb17df5ec95b86d4c8
elcarim5efil <elcarim5efil@gmail.com>
2019-07-22 17:56:44 +0800

    test: update find-package-json test

--- test/units/find-package-json.test.js
+++ test/units/find-package-json.test.js
@@ -1,7 +1,10 @@
 const path = require('path');
-const { findPackageJsonPath } = require('../../src/find-package-json');
+const {
+  findPackageJsonPath,
+  findPackageJson,
+} = require('../../src/find-package-json');
 
-describe('findPackageJson', () => {
+describe('findPackageJsonPath', () => {
   test('find package.json in dir', () => {
     const actualPath = path.resolve(__dirname, '../fixtures/modules/package.json');
     const root = path.resolve(__dirname, '../fixtures/modules/dir-module/index.js');
@@ -14,6 +17,28 @@ describe('findPackageJson', () => {
     const root = path.resolve(__dirname, '../fixtures/modules/');
     const res = findPackageJsonPath(root);
     expect(res).toBe(actualPath);
+  });
+
+  test('cannot find package.json in /', () => {
+    const root = '/';
+    const res = findPackageJsonPath(root);
+    expect(res).toBeNull();
+  });
+});
+
+describe('findPackageJson', () => {
+  test('find package.json in dir', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/dir-module/index.js');
+    const res = findPackageJson(root);
+    expect(res.version).toBe('0.0.1');
+    expect(res.name).toBe('puff.test.modules.package.json');
+  });
+
+  test('find package.json in package.json root', () => {
+    const root = path.resolve(__dirname, '../fixtures/modules/');
+    const res = findPackageJson(root);
+    expect(res.version).toBe('0.0.1');
+    expect(res.name).toBe('puff.test.modules.package.json');
   });
 
   test('cannot find package.json in /', () => {
